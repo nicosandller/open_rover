40ecf1081fcd71c401f8c75cb5b098f32516fc3e
Nicolas Sandller <nicosandller@gmail.com>
2024-08-03 18:20:59 +0900

    changes default config file

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ channels = 3            # number of channels for an RGB image
 
 # Frame configuration
 frame_count = 0         # Counter used to track which frames to skip
-frames_to_skip = 10     # Number of frames to skip classification
-fps = '7'              # number of frames per seconds to get from camera sensor
+frames_to_skip = 5     # Number of frames to skip classification
+fps = '15'              # number of frames per seconds to get from camera sensor
 
 # project configuration
 project_id = "italian_parsley"
