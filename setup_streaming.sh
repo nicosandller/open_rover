14c6f9c1c2bdda9501f8609d4c8d84e1e781502d
Nicolas Sandller <nicosandller@gmail.com>
2024-08-06 21:02:29 +0900

    updates bash scripts to work in multi OD

--- setup_streaming.sh
+++ setup_streaming.sh
@@ -1,31 +1,30 @@
 #!/bin/bash
 
-# Update the package list
-sudo apt-get update
-
-# Install Python 3 and virtual environment package
-# sudo apt-get install -y python3 python3-venv
-
-# Install libcamera and necessary tools
-sudo apt-get install -y libcamera-apps
-
-# Install ffmpeg for video handling capabilities (optional but recommended)
-sudo apt-get install -y ffmpeg
-
-# install for use in pyaudio
-sudo apt-get install portaudio19-dev python3-pyaudio
-
-#re source bash
-source ~/.bashrc
+# Different models for different OS
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    # nothing
+    echo "Setting up for MacOS"
+else
+    echo "Setting up for Linux"
+    # Update the package list
+    sudo apt-get update
+    # Install Python 3 and virtual environment package
+    # sudo apt-get install -y python3 python3-venv
+    # Install libcamera and necessary tools
+    sudo apt-get install -y libcamera-apps
+    # Install ffmpeg for video handling capabilities (optional but recommended)
+    sudo apt-get install -y ffmpeg
+    # install for use in pyaudio
+    sudo apt-get install portaudio19-dev python3-pyaudio
+    #re source bash
+    source ~/.bashrc
+fi
 
 # Create a virtual environment
 python3 -m venv venv
-
 # Activate the virtual environment
 source venv/bin/activate
-
 # Install Flask and others inside the virtual environment
 pip install -r requirements.txt
-
 # Print completion message
 echo "Setup complete!!"
--- start_stream.sh
+++ start_stream.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
+# Different models for different OS
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    # macOS specific commands
+    echo "Starting stream for MacOS"
+    MODEL_PATH="/Users/nicolassandller/repos/camera_streamer/model_files/model_mac.eim"
+else
+    echo "Starting stream for Linux"
+    # Linux specific commands
+    MODEL_PATH="/home/nico/camera_streamer/model_files/model_linux.eim"
+fi
+
 # Activate the virtual environment
 echo "Sourcing python virtual environment."
 source venv/bin/activate
-# Stream
 echo "Starting stream..."
-python streamer.py /home/nico/camera_streamer/model.eim
+python streamer.py $MODEL_PATH
