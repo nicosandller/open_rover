743f25b4d6036d1018e07e10a857710b59c49f58
Nicolas Sandller <nicosandller@gmail.com>
2024-08-06 21:22:07 +0900

    removes legacy unused code

--- config.py
+++ config.py
@@ -4,12 +4,8 @@ height = 540
 channels = 3            # number of channels for an RGB image
 
 # Frame configuration
-frame_count = 0         # Counter used to track which frames to skip
-frames_to_skip = 5     # Number of frames to skip classification
+frames_to_skip = 2     # Number of frames to skip classification
 fps = '15'              # number of frames per seconds to get from camera sensor
-
-# project configuration
-project_id = "italian_parsley"
 
 # detection
 detection_threshold = 0.0
