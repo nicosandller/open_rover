8cc31a2027eb4cc2fcf5dfce7ff5b6cf0331aef2
Nicolas Sandller <nicosandller@gmail.com>
2024-08-29 18:02:33 +0900

    adds more tests for spin

--- motor_diff.py
+++ motor_diff.py
@@ -262,21 +262,32 @@ if __name__ == "__main__":
         motor._timed_move(linear_velocity=1, angular_velocity=0, seconds=0.5)
         time.sleep(2)
 
-        print("Test 2: Move forward with left turn")
+        print("Test 2: Move backward with no angular velocity")
+        motor._timed_move(linear_velocity=-1, angular_velocity=0, seconds=0.5)
+        time.sleep(2)
+
+        print("Test 3: Move forward with left turn")
         motor._timed_move(linear_velocity=1, angular_velocity=3, seconds=0.5)
         time.sleep(2)
 
-        print("Test 3: Move backwards back in place turning left")
+        print("Test 4: Move backwards back in place turning left")
         motor._timed_move(linear_velocity=-1, angular_velocity=-3, seconds=0.5)
         time.sleep(2)
 
-        # same tests but to the right
-        print("Test 4: Move forward with right turn")
+        print("Test 5: Move forward with right turn")
         motor._timed_move(linear_velocity=1, angular_velocity=-3, seconds=0.5)
         time.sleep(2)
 
-        print("Test 5: Move backwards back in place turning right")
+        print("Test 6: Move backwards back in place turning right")
         motor._timed_move(linear_velocity=-1, angular_velocity=3, seconds=0.5)
+        time.sleep(2)
+
+        print("Test 7: Spin left")
+        motor.spin(angular_velocity=-3)
+        time.sleep(2)
+
+        print("Test 8: Spin right")
+        motor.spin(angular_velocity=3)
         time.sleep(2)
 
         print("Finished tests!")
